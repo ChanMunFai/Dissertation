852b778dec3c3dd9a9b76cf782226c8591125374
Mun Fai <mc821@ic.ac.uk>
2022-08-29 21:01:12 +0100

    update utils

--- utils.py
+++ utils.py
@@ -1,12 +1,5 @@
 import os 
 
-def checkdir(directory):
-    """ Creates file directory if it does not yet exist. 
-    """
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        print('Make dir: %s'%directory)
-
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
